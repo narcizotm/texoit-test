9904023a768056be78a349338d61d66d7499b48e
Narcizo <devnarcizo@gmail.com>
2024-04-16 11:28:47 -0300

    integrada com a API a tabela Os três estúdios com mais vitórias

--- src/Views/Dashboard/Top3StudiosWinners/index.tsx
+++ src/Views/Dashboard/Top3StudiosWinners/index.tsx
@@ -1,31 +1,65 @@
-import { Card, Table } from "react-bootstrap"
+import { useEffect, useState } from "react";
+import { Card, Table } from "react-bootstrap";
+
+const API_URL = import.meta.env.VITE_API_URL_BASE;
+
+interface Top3Winners {
+  name: string;
+  winCount: number;
+}
 
 const Top3StudiosWinners = () => {
+  const [winners, setWinners] = useState<Top3Winners[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchWinners = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        `${API_URL}?projection=studios-with-win-count`
+      );
+      const winners = await response.json();
+      setWinners(winners.studios as Top3Winners[]);
+      setIsLoading(false);
+    };
+
+    fetchWinners();
+  }, []);
+
   return (
     <Card className="mt-3">
       <Card.Title className="m-3">Top 3 Studios With Winners</Card.Title>
       <Card.Body>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Win Count</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Columbia</td>
-              <td>6</td>
-            </tr>
-            <tr>
-              <td>Paramount</td>
-              <td>6</td>
-            </tr>
-          </tbody>
-        </Table>
+        {isLoading ? (
+          <p>carregando...</p>
+        ) : Array.isArray(winners) ? (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Win Count</th>
+              </tr>
+            </thead>
+            <tbody>
+              {winners
+                .sort((a, b) => (a.winCount > b.winCount ? -1 : 1))
+                .slice(0, 3)
+                .map((winner, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{winner.name}</td>
+                      <td>{winner.winCount}</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </Table>
+        ) : (
+          <p className="text-danger">Não foi possível listar as informações</p>
+        )}
       </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default Top3StudiosWinners
+export default Top3StudiosWinners;
