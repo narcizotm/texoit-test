71623538ff2871652a8126e6f9a36a3a7777c77d
Narcizo <devnarcizo@gmail.com>
2024-04-16 10:05:58 -0300

    integrada com a API a tabela Anos que tiveram mais de um vencedor; adicionada variável de ambiente para URL base da API

--- src/Views/Dashboard/YearsMultipleWinners/index.tsx
+++ src/Views/Dashboard/YearsMultipleWinners/index.tsx
@@ -1,31 +1,62 @@
-import { Card, Table } from "react-bootstrap"
+import { useEffect, useState } from "react";
+import { Card, Table } from "react-bootstrap";
 
-const YearsMultipleWinners = () => {
-  return (
-    <Card className="mt-3">
-      <Card.Title className="m-3">List Years With Multiple Winners</Card.Title>
-      <Card.Body>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Year</th>
-              <th>Win Count</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1986</td>
-              <td>2</td>
-            </tr>
-            <tr>
-              <td>1990</td>
-              <td>2</td>
-            </tr>
-          </tbody>
-        </Table>
-      </Card.Body>
-    </Card>
-  )
+const API_URL = import.meta.env.VITE_API_URL_BASE;
+
+interface MultipleWinners {
+  year: number;
+  winnerCount: number;
 }
 
-export default YearsMultipleWinners
+const YearsMultipleWinners = () => {
+  const [movies, setMovies] = useState<MultipleWinners[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        `${API_URL}?projection=years-with-multiple-winners`
+      );
+      const movies = await response.json();
+      setMovies(movies.years as MultipleWinners[]);
+      setIsLoading(false);
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <Card className="mt-3">
+      <Card.Title className="m-3">List years with multiple winners</Card.Title>
+      <Card.Body>
+        {isLoading ? (
+          <p>carregando...</p>
+        ) : Array.isArray(movies) ? (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Year</th>
+                <th>Win Count</th>
+              </tr>
+            </thead>
+            <tbody>
+              {movies.map((movie, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{movie.year}</td>
+                    <td>{movie.winnerCount}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        ) : (
+          <p className="text-danger">Não foi possível listar as informações</p>
+        )}
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default YearsMultipleWinners;
